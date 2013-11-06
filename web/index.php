864ff869ce6627a5d9299f8a6a830c606fd2644f
NOLot <martenweijer@gmail.com>
2013-11-06 16:03:23 +0100

    Set debug true to debug easily

--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ .'/../vendor/autoload.php';
 
 $app = new Sarracenia\App();
+$app['debug'] = true;
 $app->register(new Dionaea\Modules\ModulesExtension(__DIR__ .'/../src'));
 
 $app->dispatch();
