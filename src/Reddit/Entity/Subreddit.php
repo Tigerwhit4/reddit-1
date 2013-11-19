3ca0e3820d8be6ee2341792bb204f32894c880c1
NOLot <martenweijer@gmail.com>
2013-11-19 14:01:32 +0100

    Update to the latest version

--- src/Reddit/Entity/Subreddit.php
+++ src/Reddit/Entity/Subreddit.php
@@ -4,7 +4,6 @@ namespace Reddit\Entity;
 
 /**
  * @Table(subreddits)
- * @Timestamps
  */
 class Subreddit
 {
@@ -21,14 +20,4 @@ class Subreddit
      * @Relation(type=OneToMany, target=Reddit:Topic, column=subreddit_id)
      */
     public $topics;
-
-    /**
-     * @Timestamp(created)
-     */
-    public $createdAt;
-
-    /**
-     * @Timestamp(updated)
-     */
-    public $updatedAt;
 }
